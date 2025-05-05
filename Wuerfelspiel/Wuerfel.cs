da0cdfcb109b49b17e5ba97058e4be9aa5cb657f
maximilian scheuermann <maximilian.s117@gso.schule.koeln>
2025-05-05 14:13:09 +0200

    Türken Leck Kuersat

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -4,5 +4,11 @@ namespace Wuerfelspiel
 {
     public class Wuerfel
     {
+        private int seiten;
+        public Wuerfel(int seiten){
+            this.seiten = seiten;
+            }
+
+        public int Seiten { get { return seiten; } }
     }
 }
